9dc3729822f07e2b3c9601f019ad08b4534e73b2
ito-ryo-50a <1210riytoo@gmail.com>
2024-05-03 11:40:32 +0900

    カラーテーマ設定しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,32 @@ module.exports = {
   ],
   daisyui: {
     darkTheme: false,
-  }
+    themes: [
+      {
+        'mytheme': {
+          'primary': '#FC7400',
+          'secondary': '#695954',
+          'background': '#F9F7F4',
+          'surface': '#FFFFFF',
+          'error': '#FF0000',
+          'success': '#17DB4E',
+          'warning': '#FAB515',
+        },
+      }
+    ]
+  },
+  theme: {
+    extend: {
+      colors: {
+        'app-primary': '#FC7400',
+        'app-primary-contrast': '#FFFFFF',
+        'app-secondary': '#695954',
+        'app-surface': '#FFFFFF',
+        'app-background': '#F9F7F4',
+        'app-danger': '#FF0000',
+        'app-success': '17DB4E',
+        'app-alert': '#FAB515',
+      },
+    },
+  },
 }
