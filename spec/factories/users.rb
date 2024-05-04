9f2471f8d0bcc40f720174a043361df95bbea44b
ito-ryo-50a <1210riytoo@gmail.com>
2024-05-05 00:30:06 +0900

    Userモデル　モデルスペック記述しました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    seaquence(:email) { |n| "for#{n}@example.com" }
+    seaquence(:nickname) { |n| "nickname#{n}" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,19 +18,86 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "モデルのテストでニックネームがあれば有効な状態であること"
-  it "モデルのテストでニックネームがなければ無効な状態であること"
-  it "モデルのテストでニックネームが20文字ジャストであれば有効な状態であること"
-  it "モデルのテストでニックネームが19文字であれば有効な状態であること"
-  it "モデルのテストでニックネームが21文字以上であれば無効な状態であること"
-  it "モデルのテストでメールアドレスがあれば有効な状態であること"
-  it "モデルのテストでメールアドレスがなければ無効な状態であること"
-  it "モデルのテストでメールアドレスが255文字ジャストであれば有効な状態であること"
-  it "モデルのテストでメールアドレスが254文字であれば有効な状態であること"
-  it "モデルのテストでメールアドレスが256文字以上であれば無効な状態であること"
-  it "モデルのテストでパスワードがあれば有効な状態であること"
-  it "モデルのテストでパスワードがなければ無効な状態であること"
-  it "モデルのテストでパスワードが8文字ジャストであれば有効な状態であること"
-  it "モデルのテストでパスワードが7文字であれば無効な状態であること"
-  it "モデルのテストでパスワードが9文字以上であれば有効な状態であること"
+  let(:user) { build(:user) }
+
+  it "モデルのテストでニックネームがあれば有効な状態であること" do
+    user = User.new(nickname: "nickname")
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでニックネームがなければ無効な状態であること" do
+    user = User.new(nickname: nil)
+    user.valid?
+    expect(user.errors[:nickname]).to include("can't be blank")
+  end
+
+  it "モデルのテストでニックネームが20文字ジャストであれば有効な状態であること" do
+    user = User.new(nickname: "a" * 20)
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでニックネームが19文字であれば有効な状態であること" do
+    user = User.new(nickname: "a" * 19)
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでニックネームが21文字以上であれば無効な状態であること" do
+    user = User.new(nickname: "a" * 21)
+    user.valid?
+    expect(user.errors[:nickname]).to include("is too long (maximum is 20 characters)")
+  end
+
+  it "モデルのテストでメールアドレスがあれば有効な状態であること" do
+    user = User.new(email: "for@exemple.com")
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでメールアドレスがなければ無効な状態であること" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "モデルのテストでメールアドレスが255文字ジャストであれば有効な状態であること" do
+    user = User.new(email: "a" * 243 + "@example.com")
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでメールアドレスが254文字であれば有効な状態であること" do
+    user = User.new(email: "a" * 242 + "@example.com")
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでメールアドレスが256文字以上であれば無効な状態であること" do
+    user = User.new(email: "a" * 244 + "@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
+  end
+
+  it "モデルのテストでパスワードがあれば有効な状態であること" do
+    user = User.new(password: "password")
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでパスワードがなければ無効な状態であること" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "モデルのテストでパスワードが8文字ジャストであれば有効な状態であること" do
+    user = User.new(password: "a" * 8)
+    expect(user).to be_valid
+  end
+
+  it "モデルのテストでパスワードが7文字であれば無効な状態であること" do
+    user = User.new(password: "a" * 7)
+    user.valid?
+    expect(user.errors[:password]).to include("is too short (minimum is 8 characters)")
+  end
+
+  it "モデルのテストでパスワードが9文字以上であれば有効な状態であること" do
+    user = User.new(password: "a" * 9)
+    expect(user).to be_valid
+  end
 end
