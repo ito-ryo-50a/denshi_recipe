c084a2d564594d057a4eb236fa69fa55ef29ea94
ito-ryo-50a <1210riytoo@gmail.com>
2024-05-05 18:40:44 +0900

    rubocopの指摘事項Lint修正しました

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,8 @@
 class UserSessionsController < ApplicationController
-
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
-    Rails.logger.debug("Login result: #{@user.inspect}")
     if @user
       flash[:notice] = 'ログインしました'
       redirect_back_or_to root_path
