7217c50bfe4097a8f55437a70543a56a61bb84fe
ito-ryo-50a <1210riytoo@gmail.com>
2024-05-16 23:19:55 +0900

    画面追加しました

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top how_to_use faq term_of_service privacy_policy support contact]
   def top; end
+
+  def how_to_use; end
+
+  def faq; end
+
+  def term_of_service; end
+
+  def privacy_policy; end
+
+  def support; end
+
+  def contact; end
 end
