40ebc784a3aeb70fa4fe1a34f4420675458d63d9
ito-ryo-50a <1210riytoo@gmail.com>
2024-05-16 18:50:00 +0900

    コメント機能改修しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy]
   def create
     comment = current_user.comments.build(comment_params)
     if comment.save
@@ -9,14 +10,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = current_user.comments.find(params[:id])
-    comment.destroy!
-    redirect_to recipe_path(comment.recipe), notice: 'コメントを削除しました', status: :see_other
+    @comment.destroy!
+    redirect_to recipe_path(@comment.recipe), notice: 'コメントを削除しました', status: :see_other
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content).merge(recipe_id: params[:recipe_id])
+  end
+
+  def set_comment
+    @comment = current_user.comments.find(params[:id])
   end
 end
